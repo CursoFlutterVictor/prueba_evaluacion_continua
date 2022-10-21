37b8e1c7969510e962a82461cc7ece84e49f1f3e
CursoFlutterVictor <cursofluttervictor@gmail.com>
2022-10-21 11:31:03 +0200

    edito

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:prueba_evaluacion_continua/ejercicio_1/ejercicio_1.dart';
 import 'package:prueba_evaluacion_continua/ejercicio_2/ejercicio_2.dart';
 
 void main(List<String> args) {
+  // Ejecuta los ejercicios
+  // El ejericio 3 no tiene prueba, ya que solo es la definicion de las clases.
   ejercicio_1();
   ejercicio_2();
 }
